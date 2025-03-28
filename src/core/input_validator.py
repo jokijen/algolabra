a84a64793d3dd1b0a7727ea1f83676a54683d6b4
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2025-03-28 13:40:57 +0200

    Add first methods for validating parts of user input

--- src/core/input_validator.py
+++ src/core/input_validator.py
@@ -1,3 +1,5 @@
+import math
+from .exceptions import InvalidExpressionException
 """
 Checks validity of the user's input. Either accepts it and returns an optimal
 form of the expression or gives an error
@@ -20,11 +22,52 @@ class InputValidator():
         self.operators = set(["+", "-", "*", "/", "^", ])
         self.functions = set(["sin", "sqrt", "min", "max"])
         self.characters = set(["(", ")", "."])
-        self.variables = user_variables
+        self.allowed_start_chars = set(["s", "m", "-", "(", "."])
+        self.allowed_end_chars = set([")", "."])
+        self.user_variables = user_variables
         self.bracket_equality = 0
 
-    def validate(self, user_input:str):
-        pass
 
-    def validate_first_and_last(self, character, first: bool):
-        pass
+    def validate_expression(self, user_expression:str):
+        # Takes mathematical expression as input
+        # Returns the expression as a list of tokens
+        valid_tokens = []
+        user_expression = user_expression.strip()
+
+        # Validate length and first/last character
+        self.validate_length(user_expression)
+        self.validate_first_character(user_expression)
+        self.validate_last_character(user_expression)
+
+        # Iterate over expression and add valid tokens to valid_tokens list
+        previous_char = user_expression[0]
+        previous_type = type(previous_char)
+
+        for i in range(1, len(user_expression)+1):
+            pass
+
+
+        return valid_tokens
+
+
+    def validate_length(self, user_expression):
+        if len(user_expression) < 3:
+            raise InvalidExpressionException("The expression is too short")
+
+
+    def validate_first_character(self, user_expression):
+        first_char = user_expression[0]
+
+        if not first_char.isdigit():
+            if not first_char in self.user_variables:
+                if not first_char in self.allowed_start_chars:
+                    raise InvalidExpressionException("Invalid first character")
+
+
+    def validate_last_character(self, user_expression):
+        last_char = user_expression[-1]
+
+        if not last_char.isdigit():
+            if not last_char in self.user_variables:
+                if not last_char in self.allowed_end_chars:
+                    raise InvalidExpressionException("Invalid last character")
